d720ce0441afeb77ecc8391dee19ba84b34fe489
phungpdhe189026 <pdphung27@gmail.com>
2025-10-09 17:24:54 +0700

    phungpd-admincontroller

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public final class DBConnection {
 
-    private DBConnection() {
+    DBConnection() {
         throw new UnsupportedOperationException("Utility class");
     }
 
